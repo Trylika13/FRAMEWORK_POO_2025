1de7547bc26947502ac64c92553b5f34dc311b83
Trylika <jean.g.dev@gmail.com>
2025-12-02 08:54:44 +0100

    Model générique avec lazy 1-N

--- app/models/Book.php
+++ app/models/Book.php
@@ -4,39 +4,11 @@
 namespace App\Models;
 
 
-class Book
+class Book extends \Core\Model
 {
-    public $id, $isbn, $cover, $title, $resume, $author_id, $category_id, $created_at;
+    public $isbn, $cover, $title, $resume, $author_id, $category_id;
 
 
-    // Liaisons
-    private $author, $category;
-
-
-    public function __get(string $prop): mixed
-    {
-
-        $setterName = "set" . ucfirst($prop);
-        if (method_exists($this, $setterName)):
-            $this->$setterName();
-            return $this->$prop;
-        endif;
-        return true;
-    }
-
-
-    public function setAuthor(): void
-    {
-
-        if (!$this->author):
-            $this->author = AuthorsRepository::findOneById($this->author_id);
-
-        endif;
-    }
-    public function setCategory(): void
-    {
-        if (!$this->category):
-            $this->category = CategoriesRepository::findOneById($this->category_id);
-        endif;
-    }
+    // Liaisons 1-N
+    protected $author, $category;
 }
